d61acd6a7b06d61680d3ad58bbf6eb32c2ef3858
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-20 22:44:34 -0600

    added sections in the portal

--- apps/client/src/components/header/header.component.ts
+++ apps/client/src/components/header/header.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  imports: [CommonModule],
   templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
   standalone: true,
 })
 export class HeaderComponent {
@@ -16,8 +19,25 @@ export class HeaderComponent {
     accountConfirmToken: 'sample-token', // Example value
   };
 
+  dropdownOpen = false; // Tracks if the dropdown is open
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
   logout() {
     console.log('User logged out');
     this.user = null; // Simulate logout
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    console.log('Scrolled');
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 50) {
+      header?.classList.add('scrolled');
+    } else {
+      header?.classList.remove('scrolled');
+    }
   }
 }
