35d8abd955848d76bb546927ff0fbb578db4c932
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-20 23:39:11 -0600

    removed old.featherly

--- apps/client/src/app.component.ts
+++ apps/client/src/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 
 import { Title } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
 })
 export class AppComponent {
   constructor(private titleService: Title) {
--- apps/client/src/app.routes.ts
+++ apps/client/src/app.routes.ts
@@ -1,12 +1,7 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const appRoutes: Route[] = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'portal',
-    loadChildren: () =>
-      import('./pages/portal/portal.routes').then((m) => m.portalRoutes),
-  },
+  { path: '', component: LandingComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
