bc8a0fd97a4bc45a78c066a99822c51109866132
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-20 22:56:03 -0600

    added footer

--- apps/client/src/pages/portal/portal.component.ts
+++ apps/client/src/pages/portal/portal.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-portal',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './portal.component.html',
   styleUrl: './portal.component.css',
   encapsulation: ViewEncapsulation.None,
