a7274560b451252900591cc7ca8a4c8fb188d036
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-20 23:50:23 -0600

    added login page

--- apps/client/src/app.routes.ts
+++ apps/client/src/app.routes.ts
@@ -1,7 +1,9 @@
 import { Route } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
