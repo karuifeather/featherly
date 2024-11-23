0d2832eb47d023c19f533df154f35f9d2ad576ea
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-22 21:06:28 -0600

    pages scrolled to top when navigated

--- apps/client/src/app.config.ts
+++ apps/client/src/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(
+      appRoutes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+    ),
   ],
 };
