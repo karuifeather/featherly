660bb6282531a3b7dcf944b7551631a808486d35
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-22 18:13:11 -0600

    added working routes for dashboard navigation

--- apps/client/src/pages/dashboard/dashboard.routes.ts
+++ apps/client/src/pages/dashboard/dashboard.routes.ts
@@ -1,13 +1,17 @@
 import { Route } from '@angular/router';
 import { TourDetailComponent } from '../tourdetails/tour-details.component';
 import { FeedComponent } from '../feed/feed.component';
+import { HomeComponent } from '../home/home.component';
 
 export const dashboardRoutes: Route[] = [
   {
     path: '',
     children: [
-      { path: 'tours', component: FeedComponent }, // Tours feed
-      { path: 'tours/:tourSlug', component: TourDetailComponent }, // Tour details
+      { path: 'home', component: HomeComponent },
+      { path: 'tours', component: FeedComponent },
+      { path: 'tours/:tourSlug', component: TourDetailComponent },
+      // { path: 'bookings', component: BookingsComponent },
+      // { path: 'profile', component: ProfileComponent },
     ],
   },
 ];
