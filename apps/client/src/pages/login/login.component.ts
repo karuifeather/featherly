1440068a99dc8f287303b788ddffaf59387c7789
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-24 15:47:49 -0600

    added login feat. frontend

--- apps/client/src/pages/login/login.component.ts
+++ apps/client/src/pages/login/login.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    const { email, password, rememberMe } = this.loginForm.value;
+
+    this.authService.login(email, password).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+        // Handle success
+      },
+      error: (error) => {
+        console.error('Login error:', error);
+        // Handle error
+      },
+      complete: () => {
+        console.log('Login request completed.');
+        // Optional: Handle completion if needed
+      },
+    });
+  }
+}
