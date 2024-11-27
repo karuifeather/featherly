b9c9c3ac39357333b110b164ac7f47dc22a94f37
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-27 02:27:10 -0600

    added dynamic tour data with state

--- apps/client/src/pages/landing/landing.component.ts
+++ apps/client/src/pages/landing/landing.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LandingService } from './landing.service';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Tour } from '../../core/states/tour/tour.model';
+import { TourState } from '../../core/states/tour/tour.state';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
-export class LandingComponent {}
+export class LandingComponent implements OnInit {
+  private store = inject(Store);
+
+  // Observable for popular tours from the store
+  popularTours$: Observable<Tour[] | null> = this.store.select(
+    TourState.getPopularTours
+  );
+
+  constructor(private landingService: LandingService) {}
+
+  ngOnInit() {
+    this.landingService.fetchPopularTours();
+  }
+}
---
+++ apps/client/src/pages/landing/landing.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { AxiosService } from '../../core/services/axios.service';
+import { AxiosInstance } from 'axios';
+import { SetPopularTours } from '../../core/states/tour/tour.actions';
+
+@Injectable({ providedIn: 'root' })
+export class LandingService {
+  private api: AxiosInstance;
+
+  constructor(private axiosService: AxiosService, private store: Store) {
+    this.api = this.axiosService.instance;
+  }
+
+  async fetchPopularTours() {
+    try {
+      const response = await this.api.get('/tours/popular');
+      this.store.dispatch(new SetPopularTours(response.data));
+    } catch (error) {
+      console.error('Error fetching popular tours:', error);
+    }
+  }
+
+  // async fetchRecommendedTours(userId: string) {
+  //   try {
+  //     const response = await this.api.get(`/users/${userId}/recommended-tours`);
+  //     this.store.dispatch(new SetRecommendedTours(response.data));
+  //   } catch (error) {
+  //     console.error('Error fetching recommended tours:', error);
+  //   }
+  // }
+
+  // async fetchToursFeed() {
+  //   try {
+  //     const response = await this.api.get('/tours');
+  //     this.store.dispatch(new SetToursFeed(response.data));
+  //   } catch (error) {
+  //     console.error('Error fetching tours feed:', error);
+  //   }
+  // }
+}
