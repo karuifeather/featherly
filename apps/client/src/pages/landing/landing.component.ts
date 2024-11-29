d3c74db11935f1f410f3e49e5ff1e70255bf7c9c
Aashaya Aryal <aryalaashaya@gmail.com>
2024-11-29 12:11:46 -0600

    landing search now works

--- apps/client/src/pages/landing/landing.component.ts
+++ apps/client/src/pages/landing/landing.component.ts
@@ -1,20 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterConfigOptions, RouterModule } from '@angular/router';
 import { LandingService } from './landing.service';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Tour } from '../../core/states/tour/tour.model';
 import { TourState } from '../../core/states/tour/tour.state';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
 export class LandingComponent implements OnInit {
+  searchQuery = '';
   private store = inject(Store);
 
   // Observable for popular tours from the store
@@ -22,9 +24,16 @@ export class LandingComponent implements OnInit {
     TourState.getPopularTours
   );
 
-  constructor(private landingService: LandingService) {}
+  constructor(private landingService: LandingService, private router: Router) {}
 
   ngOnInit() {
     this.landingService.fetchPopularTours();
+  }
+
+  onSubmit(): void {
+    // Navigate to the search results page with the query as a parameter
+    this.router.navigate(['/search'], {
+      queryParams: { keyword: this.searchQuery },
+    });
   }
 }
